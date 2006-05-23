81509c0448a70ab61f4d8d8ac31d9744d099a783
Risto Alt <risto@tripledev.ee>
2006-05-23 13:43:12 +0000

    removed Over, Click and Out functions

--- public/script.js
+++ public/script.js
@@ -15,30 +15,6 @@ if ((navigator.appName.substring(0,5) == "Netsc"
  gTarget = 'img1';
 }
 
-function Click(Target) {
- if (Net != 1){
-  if (Target != gTarget) {
-   document[Target].src = over.src;
-   document[gTarget].src = out.src;
-   gTarget = Target;
-   gammel.src = document[Target].src;
-  }
- }
-}
-
-function Over(Target) {
- if (Net != 1){
-  gammel.src = document[Target].src;
-  document[Target].src = over.src;
- }
-}
-
-function Out(Target) {
- if (Net != 1){
-  document[Target].src = gammel.src;
- }
-}
-
 var browser = document.all ? 'E' : 'N';
 
 function login() {
