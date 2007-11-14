981e4bba4152ff034d65117682a4135ce8a8f634
Søren Roug <soren.roug@eea.europa.eu>
2007-11-14 16:11:37 +0000

    Added a test for Props.java

---
+++ src/eionet/test/PropsTest.java
@@ -0,0 +1,49 @@
+package eionet.test;
+
+import eionet.util.PropsIF;
+import eionet.util.Props;
+import junit.framework.TestCase;
+
+/**
+ * 
+ * @author Jaanus Heinlaid, e-mail: <a href="mailto:jaanus.heinlaid@tietoenator.com">jaanus.heinlaid@tietoenator.com</a>
+ *
+ */
+public class PropsTest extends TestCase {
+	
+	/** */
+	protected Props props = null;
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public PropsTest(String name) {
+		super(name);
+	}
+
+	/*
+	 *  (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		props = new Props();
+	}
+
+	/*
+	 *  (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/*
+	 * Test method for 'eionet.util.Props.isXHTMLEntity(String)'
+	 */
+	public void test_getProperty() {
+		assertEquals("com.mysql.jdbc.Driver", props.getProperty(PropsIF.DBDRV));
+	}
+
+}
