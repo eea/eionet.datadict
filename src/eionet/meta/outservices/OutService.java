3c31d204b003f306bc40bcdf1c0b72a5305ad81d
Jaanus Heinlaid <jaanus.heinlaid@gmail.com>
2009-12-17 11:02:12 +0000

    Implemented DD support for https://svn.eionet.europa.eu/projects/Reportnet/ticket/2197, requested by Marek Staron

--- src/eionet/meta/outservices/OutService.java
+++ src/eionet/meta/outservices/OutService.java
@@ -10,27 +10,37 @@ import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Vector;
 
+import eionet.meta.DDException;
 import eionet.meta.DDSearchEngine;
+import eionet.meta.Dataset;
 import eionet.meta.DsTable;
 import eionet.util.Props;
 import eionet.util.PropsIF;
+import eionet.util.Util;
 import eionet.util.sql.ConnectionUtil;
 
+/**
+ * 
+ */
 public class OutService {
-	
+
+	/** */
 	Connection conn = null;
-	
-	/*
+
+	/**
 	 * 
 	 */
 	public OutService(){
 	}
-	
-	/*
+
+	/**
 	 * 
+	 * @param raID
+	 * @return
+	 * @throws Exception
 	 */
 	public Vector getParametersByActivityID(String raID) throws Exception{
-		
+
 		try{
 			if (conn==null) getConnection();
 			DDSearchEngine searchEngine = new DDSearchEngine(conn);
@@ -41,12 +51,15 @@ public class OutService {
 		}
 	}
 
-	/*
-	 * This one returns the IDs and titles of all ogligations that have
-	 * a released dataset definition present in DD
+	/**
+	 * This method returns the IDs and titles of all ogligations that have
+	 * a released dataset definition present in DD.
+	 * 
+	 * @return
+	 * @throws Exception
 	 */
 	public Vector getObligationsWithDatasets() throws Exception{
-		
+
 		try{
 			if (conn==null) getConnection();
 			DDSearchEngine searchEngine = new DDSearchEngine(conn);
@@ -74,62 +87,160 @@ public class OutService {
 	 * @return Vector of Hashtables
 	 * @throws Exception
 	 */
-		public Vector getDSTables() throws Exception{
-			
-			try{
-				if (conn==null) getConnection();
-				DDSearchEngine searchEngine = new DDSearchEngine(conn);
-				
-				HashSet dstStatuses = new HashSet();
-				dstStatuses.add("Released");
-				dstStatuses.add("Recorded");
-				Vector result = searchEngine.getDatasetTables(null, null, null, null, null, null, dstStatuses, false);
-		
-				Vector ret = new Vector();
-				for (int i=0; i<result.size(); i++){
-					DsTable table = (DsTable)result.get(i);
+	public Vector getDSTables() throws Exception{
 
-					String table_id = table.getID();
-					String table_name = table.getShortName();
-					String ds_id = table.getDatasetID();
-					String ds_name = table.getDatasetName();
-					String dsNs = table.getParentNs();
-		
-					Hashtable hash = new Hashtable();
-					hash.put("tblId", table.getID());
-					hash.put("identifier", table.getIdentifier());
-					hash.put("shortName", table.getShortName());
-					hash.put("dataSet", table.getDatasetName());
-					
-					if (table.getDstStatus()!=null && table.getDstStatus().equals("Released") && table.getDstDate()!=null){
-						String dateFormatted = (new SimpleDateFormat("ddMMyy")).format(new Date(Long.parseLong(table.getDstDate())));
-						hash.put("dateReleased", dateFormatted);
-					}
-					ret.add(hash);			
-					
-				}		
-				return ret;
+		try{
+			if (conn==null) getConnection();
+			DDSearchEngine searchEngine = new DDSearchEngine(conn);
 
-			}
-			finally{
-				closeConnection();
-			}
-				
-		}	
-	
-	
-	/*
+			HashSet dstStatuses = new HashSet();
+			dstStatuses.add("Released");
+			dstStatuses.add("Recorded");
+			Vector result = searchEngine.getDatasetTables(null, null, null, null, null, null, dstStatuses, false);
+
+			Vector ret = new Vector();
+			for (int i=0; i<result.size(); i++){
+				DsTable table = (DsTable)result.get(i);
+
+				String table_id = table.getID();
+				String table_name = table.getShortName();
+				String ds_id = table.getDatasetID();
+				String ds_name = table.getDatasetName();
+				String dsNs = table.getParentNs();
+
+				Hashtable hash = new Hashtable();
+				hash.put("tblId", table.getID());
+				hash.put("identifier", table.getIdentifier());
+				hash.put("shortName", table.getShortName());
+				hash.put("dataSet", table.getDatasetName());
+
+				if (table.getDstStatus()!=null && table.getDstStatus().equals("Released") && table.getDstDate()!=null){
+					String dateFormatted = (new SimpleDateFormat("ddMMyy")).format(new Date(Long.parseLong(table.getDstDate())));
+					hash.put("dateReleased", dateFormatted);
+				}
+				ret.add(hash);			
+
+			}		
+			return ret;
+
+		}
+		finally{
+			closeConnection();
+		}
+
+	}	
+
+
+	/**
 	 * 
+	 * @throws Exception
 	 */
 	private void getConnection() throws Exception{
 		conn = ConnectionUtil.getSimpleConnection();
 	}
-	
-	/*
+
+	/**
 	 * 
 	 */
 	private void closeConnection(){
 		try{ if (conn!=null) conn.close(); } catch (SQLException e){}
+	}
+
+	/**
+	 * 
+	 * @param objType
+	 * @param objId
+	 * @return
+	 * @throws Exception
+	 */
+	public Hashtable getDatasetWithReleaseInfo(String objType, String objId) throws Exception{
+
+		// validate objType
+		if (objType==null || (!objType.equals("dst") && !objType.equals("tbl"))){
+			throw new IllegalArgumentException("Missing or invalid objType!");
+		}
+
+		// validate objId
+		if (objId==null || objId.trim().length()==0 || !Util.isNumericID(objId)){
+			throw new IllegalArgumentException("Missing or invalid objId!");
+		}
+
+		ReplyGetDataset reply = new ReplyGetDataset();
+		try{
+			// initiate the connection
+			if (conn==null){
+				getConnection();
+			}
+			
+			// get the dataset object
+			DDSearchEngine searchEngine = new DDSearchEngine(conn);
+			Dataset dst = null;
+			if (objType.equals("dst")){
+				dst = searchEngine.getDataset(objId);
+			}
+			else{
+				DsTable tbl = searchEngine.getDatasetTable(objId);
+				if (tbl!=null){
+					String dstId = tbl.getDatasetID();
+					if (dstId!=null){
+						dst = searchEngine.getDataset(dstId);
+					}
+				}
+			}
+			
+			// if dataset object found, prepare the reply
+			if (dst!=null){
+
+				// set basics
+				reply.setId(dst.getID());
+				reply.setIdentifier(dst.getIdentifier());
+				reply.setShortname(dst.getShortName());
+				reply.setStatus(dst.getStatus());
+				reply.setDate(dst.getDate());
+				reply.setVersion(dst.getVersion());
+				
+				// set table ids
+				Vector tables = searchEngine.getDatasetTables(dst.getID());
+				for (int i=0; i<tables.size(); i++){
+					reply.addTableId(((DsTable)tables.get(i)).getID());
+				}
+				
+				// see if this is the latest released dataset, if no then find the latest and set its id+date
+				Vector statuses = new Vector();
+				statuses.add("Released");
+				String latestReleasedDatasetId = searchEngine.getLatestDstID(dst.getIdentifier(), statuses);
+				if (latestReleasedDatasetId!=null){
+					
+					if (latestReleasedDatasetId.equals(dst.getID())){
+						reply.setIsLatestReleased(true);
+					}
+					else{
+						reply.setIsLatestReleased(false);
+						Dataset latestReleasedDst = searchEngine.getDataset(latestReleasedDatasetId);
+						if (latestReleasedDst!=null){
+							reply.setIdOfLatestReleased(latestReleasedDst.getID());
+							reply.setDateOfLatestReleased(latestReleasedDst.getDate());
+						}
+					}
+				}
+			}
+		}
+		finally{
+			closeConnection();
+		}
+		
+		return reply.getHashTable();
+	}
+	
+	/**
+	 * 
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception{
+		
+		OutService outService = new OutService();
+		System.out.println(outService.getDatasetWithReleaseInfo("dst", "2807"));
 	}
 }
 
---
+++ src/eionet/meta/outservices/ReplyGetDataset.java
@@ -0,0 +1,130 @@
+package eionet.meta.outservices;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+/**
+ * 
+ */
+public class ReplyGetDataset{
+	
+	/** */
+	private static final String ID = "id";
+	private static final String IDENTIFIER = "identifier";
+	private static final String SHORTNAME = "shortname";
+	private static final String STATUS = "status";
+	private static final String VERSION = "version";
+	private static final String DATE = "date";
+	private static final String TABLE_IDS = "tableIds";
+	
+	/** */
+	private static final String IS_LATEST_RELEASED = "isLatestReleased";
+	private static final String ID_OF_LATEST_RELEASED = "idOfLatestReleased";
+	private static final String DATE_OF_LATEST_RELEASED = "dateOfLatestReleased";
+
+	/** */
+	private Hashtable hashTable = new Hashtable();
+	
+	/**
+	 * 
+	 */
+	public ReplyGetDataset(){
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(String id){
+		hashTable.put(ID, id);
+	}
+
+	/**
+	 * 
+	 * @param identifier
+	 */
+	public void setIdentifier(String identifier){
+		hashTable.put(IDENTIFIER, identifier);
+	}
+
+	/**
+	 * 
+	 * @param shortname
+	 */
+	public void setShortname(String shortname){
+		hashTable.put(SHORTNAME, shortname);
+	}
+	
+	/**
+	 * 
+	 * @param status
+	 */
+	public void setStatus(String status){
+		hashTable.put(STATUS, status);
+	}
+
+	/**
+	 * 
+	 * @param version
+	 */
+	public void setVersion(String version){
+		hashTable.put(VERSION, version);
+	}
+	
+	/**
+	 * 
+	 * @param dateReleased
+	 */
+	public void setDate(String date){
+		hashTable.put(DATE, date);
+	}
+	
+	/**
+	 * 
+	 * @param f
+	 */
+	public void setIsLatestReleased(boolean f){
+		hashTable.put(IS_LATEST_RELEASED, String.valueOf(f));
+	}
+	
+	/**
+	 * 
+	 * @param tableId
+	 */
+	public void addTableId(String tableId){
+		
+		if (tableId!=null && tableId.trim().length()>0){
+
+			Vector tableIds = (Vector)hashTable.get(TABLE_IDS);
+			if (tableIds==null){
+				tableIds = new Vector();
+				hashTable.put(TABLE_IDS, tableIds);
+			}
+
+			tableIds.add(tableId);
+		}
+	}
+
+	/**
+	 * 
+	 * @param idOfLatestReleased
+	 */
+	public void setIdOfLatestReleased(String idOfLatestReleased){
+		hashTable.put(ID_OF_LATEST_RELEASED, idOfLatestReleased);
+	}
+
+	/**
+	 * 
+	 * @param dateOfLatestReleased
+	 */
+	public void setDateOfLatestReleased(String dateOfLatestReleased){
+		hashTable.put(DATE_OF_LATEST_RELEASED, dateOfLatestReleased);
+	}
+
+	/**
+	 * @return the hashTable
+	 */
+	public Hashtable getHashTable() {
+		return hashTable;
+	}
+}
