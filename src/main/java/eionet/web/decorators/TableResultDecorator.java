4fe3ef4a474c2f506601f7229118b4b784a3fe81
Juhan Voolaid <juhan@tripledev.ee>
2011-09-27 14:08:13 +0000

    Task #2743 - recreated search tables view functionality in Stripes framework.

---
+++ src/main/java/eionet/web/decorators/TableResultDecorator.java
@@ -0,0 +1,61 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is Content Registry 3
+ *
+ * The Initial Owner of the Original Code is European Environment
+ * Agency. Portions created by TripleDev or Zero Technologies are Copyright
+ * (C) European Environment Agency.  All Rights Reserved.
+ *
+ * Contributor(s):
+ *        Juhan Voolaid
+ */
+
+package eionet.web.decorators;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.displaytag.decorator.TableDecorator;
+
+import eionet.dao.domain.DataSetTable;
+import eionet.meta.DDUser;
+import eionet.util.SecurityUtil;
+
+/**
+ * Table result table decorator.
+ *
+ * @author Juhan Voolaid
+ */
+public class TableResultDecorator extends TableDecorator {
+
+    public String getName() {
+        DataSetTable table = (DataSetTable) getCurrentRowObject();
+        String status = table.getDataSetStatus();
+        boolean clickable = false;
+
+        if (status != null) {
+            DDUser user = SecurityUtil.getUser((HttpServletRequest) getPageContext().getRequest());
+            if (user != null && user.isAuthentic()) {
+                if (status.equals("Incomplete") || status.equals("Candidate") || status.equals("Qualified"))
+                    clickable = true;
+            }
+        }
+
+        if (clickable) {
+            String url =
+                    "dstable.jsp?table_id=" + table.getId() + "&amp;ds_id=" + table.getDataSetId() + "&amp;ds_name="
+                            + table.getDataSetName();
+            return "<a href=\"" + url + "\">" + table.getName() + "</a>";
+        }
+
+        return table.getName();
+    }
+}
