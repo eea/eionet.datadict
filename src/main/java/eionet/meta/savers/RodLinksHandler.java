83bc2a2f338d7d19075be076aa3a70af99fd8438
Jaanus Heinlaid <jaanus.heinlaid@gmail.com>
2012-05-28 12:18:12 +0000

    DD now ensures that ROD obligation titles remain up-to-date in ROD_ACTIVITIES table. Fixes #3070.

--- src/main/java/eionet/meta/savers/RodLinksHandler.java
+++ src/main/java/eionet/meta/savers/RodLinksHandler.java
@@ -35,18 +35,20 @@ public class RodLinksHandler extends BaseHandler{
     public void execute_() throws Exception {
 
         String dstID = httpServletRequest.getParameter("dst_id");
-        if (dstID==null)
+        if (dstID==null) {
             throw new Exception("RodLinksHandler: dstID is missing!");
+        }
 
         String mode = httpServletRequest.getParameter("mode");
-        if (mode==null)
+        if (mode==null) {
             throw new Exception("RodLinksHandler: mode is missing!");
-        else if (mode.equals("add"))
+        } else if (mode.equals("add")) {
             addRodLinks(dstID);
-        else if (mode.equals("rmv"))
+        } else if (mode.equals("rmv")) {
             rmvRodLinks(dstID);
-        else
+        } else {
             throw new Exception("RodLinksHandler: unknown mode " + mode);
+        }
     }
 
     /**
@@ -56,8 +58,9 @@ public class RodLinksHandler extends BaseHandler{
     private void addRodLinks(String dstID) throws Exception {
 
         String raID = httpServletRequest.getParameter("ra_id");
-        if (Util.isEmpty(raID))
+        if (Util.isEmpty(raID)) {
             throw new Exception("ra_id is missing!");
+        }
 
         INParameters inParams = new INParameters();
         StringBuffer buf = new StringBuffer("select count(*) from DST2ROD where DATASET_ID=").
@@ -68,14 +71,18 @@ public class RodLinksHandler extends BaseHandler{
         try {
             stmt = SQL.preparedStatement(buf.toString(), inParams, conn);
             rs = stmt.executeQuery();
-            if (rs.next() && rs.getInt(1) > 0)
+            if (rs.next() && rs.getInt(1) > 0) {
                 throw new Exception("This dataset is already linked with this obligation!");
-            else
+            } else {
                 rs.close();
+            }
 
             String raTitle = httpServletRequest.getParameter("ra_title");
             String liID = httpServletRequest.getParameter("li_id");
             String liTitle = httpServletRequest.getParameter("li_title");
+
+            // Determine if an activity by the given ID exists in ROD_ACTIVITIES.
+            // If not, add it there. Otherwise update it there.
 
             inParams = new INParameters();
             buf = new StringBuffer("select count(*) from ROD_ACTIVITIES where ");
@@ -83,33 +90,48 @@ public class RodLinksHandler extends BaseHandler{
 
             stmt = SQL.preparedStatement(buf.toString(), inParams, conn);
             rs = stmt.executeQuery();
-
-            if (rs.next() && rs.getInt(1) == 0) {
-
-                inParams = new INParameters();
-                LinkedHashMap map = new LinkedHashMap();
-
-                map.put("ACTIVITY_ID", inParams.add(raID, Types.INTEGER));
-                if (raTitle!=null)
-                    map.put("ACTIVITY_TITLE", inParams.add(raTitle));
-                if (liID!=null)
-                    map.put("LEGINSTR_ID", inParams.add(liID, Types.INTEGER));
-                if (liTitle!=null)
-                    map.put("LEGINSTR_TITLE", inParams.add(liTitle));
-
-                SQL.executeUpdate(SQL.insertStatement("ROD_ACTIVITIES", map), inParams, conn);
-            }
+            boolean activityExists = rs.next() && rs.getInt(1) > 0;
 
             inParams = new INParameters();
             LinkedHashMap map = new LinkedHashMap();
+
+            // Prepare map for the up-coming activity insert/update statement.
+            if (raTitle!=null) {
+                map.put("ACTIVITY_TITLE", inParams.add(raTitle));
+            }
+            if (liID!=null) {
+                map.put("LEGINSTR_ID", inParams.add(liID, Types.INTEGER));
+            }
+            if (liTitle!=null) {
+                map.put("LEGINSTR_TITLE", inParams.add(liTitle));
+            }
+
+            // If new activity, add it, otherwise update.
+            if (!activityExists) {
+                map.put("ACTIVITY_ID", inParams.add(raID, Types.INTEGER));
+                SQL.executeUpdate(SQL.insertStatement("ROD_ACTIVITIES", map), inParams, conn);
+            }
+            else{
+                String sql = SQL.updateStatement("ROD_ACTIVITIES", map) + " where ACTIVITY_ID=?";
+                inParams.add(raID, Types.INTEGER);
+                SQL.executeUpdate(sql, inParams, conn);
+            }
+
+            // And now link the newly inserted/updated activity to this dataset.
+            inParams = new INParameters();
+            map = new LinkedHashMap();
             map.put("DATASET_ID", inParams.add(dstID, Types.INTEGER));
             map.put("ACTIVITY_ID", inParams.add(raID, Types.INTEGER));
             SQL.executeUpdate(SQL.insertStatement("DST2ROD", map), inParams, conn);
         }
         catch (Exception e) {
             try {
-                if (rs != null)   rs.close();
-                if (stmt != null) stmt.close();
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
             }
             catch (SQLException sqlee) {}
         }
@@ -122,15 +144,17 @@ public class RodLinksHandler extends BaseHandler{
     private void rmvRodLinks(String dstID) throws Exception {
 
         String[] raIDs = httpServletRequest.getParameterValues("del_id");
-        if (raIDs==null || raIDs.length==0)
+        if (raIDs==null || raIDs.length==0) {
             throw new Exception("ra_id is missing!");
+        }
 
         INParameters inParams = new INParameters();
         StringBuffer buf = new StringBuffer("delete from DST2ROD where DATASET_ID=");
         buf.append(dstID).append(" and (");
         for (int i=0; i<raIDs.length; i++) {
-            if (i>0)
+            if (i>0) {
                 buf.append(" or ");
+            }
             buf.append("ACTIVITY_ID=").append(inParams.add(raIDs[i], Types.INTEGER));
         }
         buf.append(")");
