ca175c003349db086edccfee5503d0089c9a6f80
Søren Roug <soren.roug@eea.europa.eu>
2013-06-22 07:14:47 +0000

    A simple unit test for FixedValue

---
+++ src/test/java/eionet/meta/FixedValueTest.java
@@ -0,0 +1,43 @@
+package eionet.meta;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Vector;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+
+/**
+ * Test the FixedValue class.
+ */
+public class FixedValueTest {
+
+    @Test
+    public void simpleStoreAndGetByShortName() {
+        String VALUE = "Ηλέκτρα";
+
+        FixedValue fv = new FixedValue("testid");
+        DElemAttribute attr = new DElemAttribute("id", "name", "shortName", "type", VALUE);
+        fv.addAttribute(attr);
+        String result = fv.getAttributeValueByShortName("shortName");
+        //String s = result.getShortName();
+
+        assertEquals(VALUE, result);
+    }
+
+    /**
+     * Is getItems() used in the production code?
+     */
+    @Test
+    public void simpleAddAndGetItem() {
+        FixedValue fv = new FixedValue("testid");
+        fv.addItem("Item1");
+        fv.addItem("Item2");
+        Vector itemlist = fv.getItems();
+        assertEquals(2, itemlist.size());
+    }
+}
