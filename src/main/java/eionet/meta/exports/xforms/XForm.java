d7a9c2aaacde46f2253e12add99ccffbb8e67b92
heinlja <jaanus.heinlaid@gmail.com>
2013-10-15 15:44:44 +0300

    Bugfix: escape content between tags in the generated XForms.

--- src/main/java/eionet/meta/exports/xforms/XForm.java
+++ src/main/java/eionet/meta/exports/xforms/XForm.java
@@ -8,6 +8,8 @@ import java.net.URL;
 import java.util.Hashtable;
 import java.util.Vector;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 import eionet.meta.DDSearchEngine;
 import eionet.meta.FixedValue;
 import eionet.util.Util;
@@ -20,7 +22,7 @@ public abstract class XForm implements XFormIF {
 
     private String instance = null;
     private String controlsLabel = "";
-    protected Vector binds    = new Vector();
+    protected Vector binds = new Vector();
     private Vector controls = new Vector();
 
     private String instanceDOM = null;
@@ -33,10 +35,12 @@ public abstract class XForm implements XFormIF {
         this.writer = writer;
     }
 
+    @Override
     public void setAppContext(String appContext) {
         if (appContext != null) {
-            if (!appContext.endsWith("/"))
+            if (!appContext.endsWith("/")) {
                 appContext = appContext + "/";
+            }
             this.appContext = appContext;
         }
     }
@@ -58,20 +62,23 @@ public abstract class XForm implements XFormIF {
     }
 
     /**
-    * Flush the written content into the writer.
-    */
+     * Flush the written content into the writer.
+     */
+    @Override
     public void flush(String template) throws Exception {
 
-        if (template == null)
+        if (template == null) {
             throw new Exception("Template path cannot be null!");
+        }
 
         URL url = new URL(template);
         InputStream in = url.openStream();
 
         String line;
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        while ((line = reader.readLine()) != null)
+        while ((line = reader.readLine()) != null) {
             writeLine(line);
+        }
 
         in.close();
         reader.close();
@@ -79,7 +86,9 @@ public abstract class XForm implements XFormIF {
 
     private void writeLine(String line) throws Exception {
 
-        if (line == null) return;
+        if (line == null) {
+            return;
+        }
 
         if (line.trim().startsWith("<f:model")) {
             writer.println(line);
@@ -96,29 +105,32 @@ public abstract class XForm implements XFormIF {
             writeInsert(line);
         } else if (line.trim().startsWith("<f:delete")) {
             writeDelete(line);
-        } else
+        } else {
             writer.println(line);
+        }
     }
 
     protected String extractLead(String line) {
         StringBuffer buf = new StringBuffer();
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
-            if (Character.isWhitespace(c))
+            if (Character.isWhitespace(c)) {
                 buf.append(line.charAt(i));
-            else
+            } else {
                 break;
+            }
         }
         return buf.toString();
     }
 
     private void writeInstance(String lead) throws Exception {
-        writer.println(lead + "<!-- Original instance in Data Dictinary (not used anymore - use default instance value with context parameter $instance) -->");
+        writer.println(lead
+                + "<!-- Original instance in Data Dictinary (not used anymore - use default instance value with context parameter $instance) -->");
         writer.println(lead + "<!--  <f:instance id=\"original\" src=\"" + instance + "\"/>  -->");
     }
 
     private void writeControlsLabel(String lead) throws Exception {
-        writer.println(lead + "<f:label>" + controlsLabel + "</f:label>");
+        writer.println(lead + "<f:label>" + escapeXml(controlsLabel) + "</f:label>");
     }
 
     protected void writeRepeat(String line) throws Exception {
@@ -145,10 +157,18 @@ public abstract class XForm implements XFormIF {
 
             StringBuffer buf = new StringBuffer("<f:bind");
 
-            if (id != null) buf.append(" id=\"").append(id).append("\"");
-            if (type != null) buf.append(" type=\"").append(type).append("\"");
-            if (nodeset != null) buf.append(" nodeset=\"").append(nodeset).append("\"");
-            if (constraint != null) buf.append(" constraint=\"").append(constraint).append("\"");
+            if (id != null) {
+                buf.append(" id=\"").append(id).append("\"");
+            }
+            if (type != null) {
+                buf.append(" type=\"").append(type).append("\"");
+            }
+            if (nodeset != null) {
+                buf.append(" nodeset=\"").append(nodeset).append("\"");
+            }
+            if (constraint != null) {
+                buf.append(" constraint=\"").append(constraint).append("\"");
+            }
 
             buf.append("/>");
 
@@ -160,34 +180,35 @@ public abstract class XForm implements XFormIF {
 
         for (int i = 0; i < controls.size(); i++) {
             Hashtable control = (Hashtable) controls.get(i);
-            String bind  = (String) control.get(ATTR_BIND);
+            String bind = (String) control.get(ATTR_BIND);
             String label = (String) control.get(CTRL_LABEL);
-            String hint  = (String) control.get(CTRL_HINT);
-            String type  = (String) control.get(CTRL_TYPE);
+            String hint = (String) control.get(CTRL_HINT);
+            String type = (String) control.get(CTRL_TYPE);
             String alert = (String) control.get(CTRL_ALERT);
 
             // start control
             StringBuffer buf = new StringBuffer("<f:").append(type);
-            if (bind != null)
+            if (bind != null) {
                 buf.append(" bind=\"").append(bind).append("\"");
+            }
             buf.append(">");
             writer.println(lead + buf.toString());
 
             // write label
             if (label != null) {
-                buf = new StringBuffer("<f:label>").append(label).append("</f:label>");
+                buf = new StringBuffer("<f:label>").append(escapeXml(label)).append("</f:label>");
                 writer.println(lead + "\t" + buf.toString());
             }
 
             // write hint
             if (hint != null) {
-                buf = new StringBuffer("<f:hint>").append(hint).append("</f:hint>");
+                buf = new StringBuffer("<f:hint>").append(escapeXml(hint)).append("</f:hint>");
                 writer.println(lead + "\t" + buf.toString());
             }
 
             // write alert
             if (alert != null) {
-                buf = new StringBuffer("<f:alert>").append(alert).append("</f:alert>");
+                buf = new StringBuffer("<f:alert>").append(escapeXml(alert)).append("</f:alert>");
                 writer.println(lead + "\t" + buf.toString());
             }
 
@@ -209,22 +230,24 @@ public abstract class XForm implements XFormIF {
     private void writeSelectItem(FixedValue fxv, String lead) throws Exception {
 
         String value = fxv.getValue();
-        if (value == null || value.length() == 0)
+        if (value == null || value.length() == 0) {
             return;
-        else
+        } else {
             value = Util.escapeXML(value);
+        }
 
         String label = value;
 
         writer.println(lead + "<f:item>");
-        writer.println(lead + "\t<f:label>" + label + "</f:label>");
-        writer.println(lead + "\t<f:value>" + value + "</f:value>");
+        writer.println(lead + "\t<f:label>" + escapeXml(label) + "</f:label>");
+        writer.println(lead + "\t<f:value>" + escapeXml(value) + "</f:value>");
         writer.println(lead + "</f:item>");
     }
 
     protected String getRef(String tagName) throws Exception {
-        if (instance == null)
+        if (instance == null) {
             throw new Exception("getRef(): instance document is not set!");
+        }
         return "";
     }
 
@@ -236,8 +259,9 @@ public abstract class XForm implements XFormIF {
         int i = line.indexOf(attrStart);
         if (i != -1) {
             int j = line.indexOf("\"", i + attrStart.length());
-            if (j != -1)
+            if (j != -1) {
                 buf.replace(i + attrStart.length(), j, value);
+            }
         }
 
         return buf.toString();
@@ -245,16 +269,20 @@ public abstract class XForm implements XFormIF {
 
     protected String escapeCDATA(String s) {
 
-        if (s == null) return null;
+        if (s == null) {
+            return null;
+        }
 
         StringBuffer buf = new StringBuffer("<![CDATA[");
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isISOControl(c)) {
-                if (Character.isWhitespace(c))
+                if (Character.isWhitespace(c)) {
                     buf.append(c);
-            } else
+                }
+            } else {
                 buf.append(c);
+            }
         }
 
         buf.append("]]>");
@@ -266,43 +294,68 @@ public abstract class XForm implements XFormIF {
         StringBuffer buf = new StringBuffer();
 
         String minSize = (String) bind.get(ATTR_MINSIZE);
-        if (minSize != null)
+        if (minSize != null) {
             buf.append("string-length(.)&gt;=").append(minSize);
+        }
 
         String maxSize = (String) bind.get(ATTR_MAXSIZE);
         if (maxSize != null) {
-            if (buf.length() != 0) buf.append(" and ");
+            if (buf.length() != 0) {
+                buf.append(" and ");
+            }
             buf.append("string-length(.)&lt;=").append(maxSize);
         }
 
         String minInclValue = (String) bind.get(ATTR_MIN_INCL_VALUE);
         String minExclValue = (String) bind.get(ATTR_MIN_EXCL_VALUE);
         if (minInclValue != null) {
-            if (buf.length() != 0) buf.append(" and ");
+            if (buf.length() != 0) {
+                buf.append(" and ");
+            }
             buf.append("number(.)&gt;=").append(minInclValue);
         } else if (minExclValue != null) {
-            if (buf.length() != 0) buf.append(" and ");
+            if (buf.length() != 0) {
+                buf.append(" and ");
+            }
             buf.append("number(.)&gt;").append(minExclValue);
         }
 
         String maxInclValue = (String) bind.get(ATTR_MAX_INCL_VALUE);
         String maxExclValue = (String) bind.get(ATTR_MAX_EXCL_VALUE);
         if (maxInclValue != null) {
-            if (buf.length() != 0) buf.append(" and ");
+            if (buf.length() != 0) {
+                buf.append(" and ");
+            }
             buf.append("number(.)&lt;=").append(maxInclValue);
         } else if (maxExclValue != null) {
-            if (buf.length() != 0) buf.append(" and ");
+            if (buf.length() != 0) {
+                buf.append(" and ");
+            }
             buf.append("number(.)&lt;").append(maxExclValue);
         }
 
-        if (buf.length() != 0)
+        if (buf.length() != 0) {
             return buf.toString();
-        else
+        } else {
             return null;
+        }
     }
 
     /*
+     * (non-Javadoc)
      *
+     * @see eionet.meta.exports.xforms.XFormIF#write(java.lang.String)
      */
+    @Override
     public abstract void write(String objID) throws Exception;
+
+    /**
+     * Convenience method for XML-escaping the given string.
+     *
+     * @param str The string to escape.
+     * @return The XML-escaped string.
+     */
+    protected String escapeXml(String str) {
+        return StringEscapeUtils.escapeXml(str);
+    }
 }
