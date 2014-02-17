b44a8fcd189f7cfa7c04277781a24141311f2822
enverveli <enverveli@gmail.com>
2014-02-17 12:42:42 +0200

    + csv import test resources are moved to a folder + test is updated to use unitils reflection assert to make it easier to compare (because dbunit compare and spring-dbunit annotations didnt work) + code formatting Refs: #18169

--- src/test/java/eionet/meta/service/CSVVocabularyImportServiceTest.java
+++ src/test/java/eionet/meta/service/CSVVocabularyImportServiceTest.java
@@ -9,61 +9,109 @@
  * implied. See the License for the specific language governing
  * rights and limitations under the License.
  *
- * The Original Code is Content Registry 3
+ * The Original Code is Data Dictionary
  *
  * The Initial Owner of the Original Code is European Environment
- * Agency. Portions created by TripleDev or Zero Technologies are Copyright
+ * Agency.  Portions created by TripleDev are Copyright
  * (C) European Environment Agency.  All Rights Reserved.
  *
  * Contributor(s):
- *        Juhan Voolaid
+ * TripleDev
  */
 
 package eionet.meta.service;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
 import org.unitils.UnitilsJUnit4;
+import org.unitils.reflectionassert.ReflectionAssert;
 import org.unitils.spring.annotation.SpringApplicationContext;
 import org.unitils.spring.annotation.SpringBeanByType;
 
+import eionet.meta.dao.domain.VocabularyConcept;
+import eionet.meta.dao.domain.VocabularyFolder;
+import eionet.util.VocabularyCSVOutputHelper;
+
 /**
- * JUnit integration test with Unitils for vocabulary service.
- * 
- * @author Juhan Voolaid
+ * JUnit integration test with Unitils for CSV Vocabulary Import Service.
+ *
+ * @author
  */
+// @DataSet(value = {"seed-vocabularycsv-import.xml"}) //TODO why does not this work!!!
+//also spring does not commit when it is working in test context so we cant utilize Dbunit compare dataset!! :(
 @SpringApplicationContext("spring-context.xml")
-// @DataSet({"seed-vocabularies.xml"})
+// @TransactionConfiguration(defaultRollback = false)
 public class CSVVocabularyImportServiceTest extends UnitilsJUnit4 {
+
+    private static final int TEST_VALID_VOCAB_FOLDER_ID = 4;
+    private static final int[] VOCAB_CONCEPT_IDS = new int[]{8, 9, 10};
 
     /** Logger. */
     protected static final Logger LOGGER = Logger.getLogger(CSVVocabularyImportServiceTest.class);
 
     @SpringBeanByType
-    private ICSVVocabularyImportService vocabularyService;
+    private ICSVVocabularyImportService vocabularyImportService;
+
+    @SpringBeanByType
+    private IVocabularyService vocabularyService;
 
     @BeforeClass
     public static void loadData() throws Exception {
-        // DBUnitHelper.loadData("seed-vocabularies.xml");
+        DBUnitHelper.loadData("csv_import/seed-vocabularycsv-import.xml");
     }
 
     @AfterClass
     public static void deleteData() throws Exception {
-        // DBUnitHelper.deleteData("seed-vocabularies.xml");
+        DBUnitHelper.deleteData("csv_import/seed-vocabularycsv-import.xml");
     }
 
     @Test
-    public void testStep1() throws Exception {
-        InputStream csvInputStream = getClass().getClassLoader().getResourceAsStream("theme_env_test.csv");
-        InputStreamReader reader = new InputStreamReader(csvInputStream);
+    @Rollback
+    //expected : seed-vocabularycsv-import-step-1-expected
+    public void testIfConceptAndElementsUpdated() throws Exception {
+        //initialize values:
+        Map<Integer, VocabularyConcept> plainVocabularyConcepts = new HashMap<Integer,VocabularyConcept>();
+        for (int vocabId : VOCAB_CONCEPT_IDS ){
+            plainVocabularyConcepts.put(vocabId, vocabularyService.getVocabularyConcept(vocabId));
+        }
 
-        //vocabularyService.importCsvIntoVocabulary(reader, null);
+        //get file and create a reader for file
+        InputStream is = getClass().getClassLoader().getResourceAsStream("csv_import/csv_import_test_1.csv");
+        byte[] firstThreeBytes = new byte[3];
+        is.read(firstThreeBytes);
+
+        if (!Arrays.equals(firstThreeBytes, VocabularyCSVOutputHelper.BOM_BYTE_ARRAY)) {
+            is.close();
+            is = getClass().getClassLoader().getResourceAsStream("csv_import/csv_import_test_1.csv");
+        }
+        InputStreamReader reader = new InputStreamReader(is);
+
+        //get vocabulary folder
+        VocabularyFolder vocabularyFolder = vocabularyService.getVocabularyFolder(TEST_VALID_VOCAB_FOLDER_ID);
+
+        //import CSV into database
+        vocabularyImportService.importCsvIntoVocabulary(reader, vocabularyFolder, false, false);
+
+        Map<Integer, VocabularyConcept> updatedPlainVocabularyConcepts = new HashMap<Integer, VocabularyConcept>();
+        for (int vocabId : VOCAB_CONCEPT_IDS ){
+            updatedPlainVocabularyConcepts.put(vocabId, vocabularyService.getVocabularyConcept(vocabId));
+        }
+
+        //do updates of objects here
+        VocabularyConcept updated = plainVocabularyConcepts.get(8);
+        updated.setLabel("csv_test_concept_label_1_updated");
+
+        ReflectionAssert.assertLenientEquals(plainVocabularyConcepts, updatedPlainVocabularyConcepts);
+
 
     }// end of test step testStep1
-
 }// end of test case CSVVocabularyImportServiceTest
