4d7cb3805d28938c53dd80657b4f504a5aba971e
enverveli <enverveli@gmail.com>
2014-04-14 19:05:34 +0300

    + languages.properties file added to test resources. testlanguagesbase.properties file added and defined as a filter in pom.xml to replace values in languages.properties. + A new test step added to test getValues method Refs: #17963

--- src/test/java/eionet/util/PropsLanguagesTest.java
+++ src/test/java/eionet/util/PropsLanguagesTest.java
@@ -28,6 +28,25 @@ public class PropsLanguagesTest {
     }
 
     @Test
+    public void testLanguagesFromLanguagesProperties() {
+        String[] labels =
+                new String[] {"Arabic", "Bulgarian", "Czech", "Danish", "German", "Greek", "English", "Spanish", "Estonian",
+                        "Finnish", "French", "Irish", "Hungarian", "Italian", "Lithuanian", "Latvian", "Maltese", "Dutch",
+                        "Norwegian", "Polish", "Portuguese", "Romanian", "Slovak", "Slovenian", "Swedish"};
+        String[] codes =
+                new String[] {"ar", "bg", "cs", "da", "de", "el", "en", "es", "et", "fi", "fr", "ga", "hu", "it", "lt", "lv",
+                        "mt", "nl", "no", "pl", "pt", "ro", "sk", "sl", "sv"};
+
+        PropsLanguages[] langs = PropsLanguages.getValues();
+
+        Assert.assertEquals("Size does not match", labels.length, langs.length);
+        for (int i = 0; i < langs.length; i++) {
+            Assert.assertEquals("Label does not match", labels[i], langs[i].getLabel());
+            Assert.assertEquals("Code does not match", codes[i], langs[i].getCode());
+        }
+    }
+
+    @Test
     public void testLanguagesWhenFileIsEmpty() {
         PropsLanguages[] langs = PropsLanguages.getValuesFrom("testlanguagesempty");
         Assert.assertEquals("Language array is not empty", 0, langs.length);
