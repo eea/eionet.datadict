c091ee8495b7efea5e3cfe251ccf652579b22ac5
enverveli <enverveli@gmail.com>
2014-05-30 18:42:41 +0300

    + java.util.zip used instead of zip command + Checkstyle updates Refs: #19357

--- src/main/java/eionet/meta/exports/ods/DstOds.java
+++ src/main/java/eionet/meta/exports/ods/DstOds.java
@@ -16,16 +16,23 @@ import eionet.meta.DsTable;
  *
  * @author jaanus
  *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ *         To change the template for this generated type comment go to Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and
+ *         Comments
  */
 public class DstOds extends Ods {
 
     /** */
     private String dstID = null;
 
-    /*
+    /**
+     * Constructor.
      *
+     * @param searchEngine
+     *            data dictionary search engine
+     * @param dstID
+     *            dataset id
+     * @throws java.lang.Exception
+     *             if operation fails
      */
     public DstOds(DDSearchEngine searchEngine, String dstID) throws Exception {
         this.searchEngine = searchEngine;
@@ -33,8 +40,11 @@ public class DstOds extends Ods {
         prepare();
     }
 
-    /*
+    /**
+     * Prepares content for dataset.
      *
+     * @throws java.lang.Exception
+     *             if operation fails
      */
     private void prepare() throws Exception {
 
@@ -42,11 +52,11 @@ public class DstOds extends Ods {
         if (dst == null)
             throw new Exception("Dataset not found: " + dstID);
 
-        finalFileName = dst.getIdentifier() + "." + DOS_EXTENSION;
+        finalFileName = dst.getIdentifier() + "." + ODS_EXTENSION;
         schemaURLTrailer = "DST" + dst.getID();
 
         Vector tbls = searchEngine.getDatasetTables(dstID, true);
-        for (int i = 0; tbls != null && i<tbls.size(); i++)
+        for (int i = 0; tbls != null && i < tbls.size(); i++)
             prepareTbl((DsTable) tbls.get(i));
     }
 }
