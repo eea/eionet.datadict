c6d74689c20c40117b33a0226b1d8dbf8c71f8eb
vasilis skiadas <vs@eworx.gr>
2016-02-26 13:23:45 +0200

    indentation fixes

--- src/main/java/eionet/meta/dao/mysql/SchemaDAOImpl.java
+++ src/main/java/eionet/meta/dao/mysql/SchemaDAOImpl.java
@@ -122,11 +122,8 @@ public class SchemaDAOImpl extends GeneralDAOImpl implements ISchemaDAO {
         params.put("checkedOutCopyId", schema.getCheckedOutCopyId() <= 0 ? null : schema.getCheckedOutCopyId());
         params.put("otherDocument", schema.isOtherDocument());
 
-       
-        
-        if (schemaExists(schema.getFileName(), schema.getSchemaSetId())==false) {
-        getNamedParameterJdbcTemplate().update(INSERT_SQL, params);
-
+        if (!schemaExists(schema.getFileName(), schema.getSchemaSetId())) {
+            getNamedParameterJdbcTemplate().update(INSERT_SQL, params);
         }
 
         return getLastInsertId();
