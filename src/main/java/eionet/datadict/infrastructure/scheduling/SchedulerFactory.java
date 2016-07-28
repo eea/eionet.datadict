b99d596d6fc9c8dacb8c95809d7205f328f63437
Nikos Nakas <nn@eworx.gr>
2016-07-28 15:48:30 +0300

    Added clustered quartz configuration - Refs #73755

--- src/main/java/eionet/datadict/infrastructure/scheduling/SchedulerFactory.java
+++ src/main/java/eionet/datadict/infrastructure/scheduling/SchedulerFactory.java
@@ -1,8 +1,10 @@
 package eionet.datadict.infrastructure.scheduling;
 
+import java.util.Properties;
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -12,11 +14,14 @@ public class SchedulerFactory extends SchedulerFactoryBean {
     
     private final DataSource dataSource;
     private final PlatformTransactionManager transactionManager;
+    private final Properties quartzProperties;
     
     @Autowired
-    public SchedulerFactory(DataSource dataSource, PlatformTransactionManager transactionManager) {
+    public SchedulerFactory(DataSource dataSource, PlatformTransactionManager transactionManager,
+            @Qualifier("quartzProperties") Properties quartzProperties) {
         this.dataSource = dataSource;
         this.transactionManager = transactionManager;
+        this.quartzProperties = quartzProperties;
     }
     
     @PostConstruct
@@ -24,6 +29,7 @@ public class SchedulerFactory extends SchedulerFactoryBean {
         this.setBeanName("jobScheduler");
         this.setDataSource(this.dataSource);
         this.setTransactionManager(this.transactionManager);
+        this.setQuartzProperties(this.quartzProperties);
     }
     
 }
