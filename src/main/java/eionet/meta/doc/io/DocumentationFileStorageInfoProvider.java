a69a2c1616fad1080a38e5ff8e811574f40886c2
aliki kop <alk@eworx.gr>
2016-09-19 13:36:32 +0300

    Configure dynamic properties for Documentation service. Refs #75535

--- src/main/java/eionet/meta/doc/io/DocumentationFileStorageInfoProvider.java
+++ src/main/java/eionet/meta/doc/io/DocumentationFileStorageInfoProvider.java
@@ -2,8 +2,8 @@ package eionet.meta.doc.io;
 
 import eionet.doc.io.FileStorageInfoProvider;
 import eionet.meta.DDRuntimeException;
+import eionet.util.Props;
 import java.io.IOException;
-import java.util.ResourceBundle;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
@@ -19,8 +19,7 @@ public final class DocumentationFileStorageInfoProvider implements FileStorageIn
     private final String fileStoragePath;
     
     public DocumentationFileStorageInfoProvider() throws IOException {
-        ResourceBundle bundle = ResourceBundle.getBundle("doc");
-        this.fileStoragePath = StringUtils.trim(bundle.getString(DOC_FOLDER_PROPERTY_NAME));
+        this.fileStoragePath = StringUtils.trim(Props.getProperty(DOC_FOLDER_PROPERTY_NAME));
         
         if (StringUtils.isBlank(fileStoragePath)) {
             String msg = String.format("Property '%s' not found", DOC_FOLDER_PROPERTY_NAME);
