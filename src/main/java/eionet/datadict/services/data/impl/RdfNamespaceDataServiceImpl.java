1ac473cd8eba56641962e2a6e6e9930e2d26259f
aliki kop <alk@eworx.gr>
2016-12-07 11:58:48 +0200

    Move services implementation to new folders. Refs #26924

---
+++ src/main/java/eionet/datadict/services/data/impl/RdfNamespaceDataServiceImpl.java
@@ -0,0 +1,25 @@
+package eionet.datadict.services.data.impl;
+
+import eionet.datadict.dal.RdfNamespaceDao;
+import eionet.datadict.model.RdfNamespace;
+import eionet.datadict.services.data.RdfNamespaceDataService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RdfNamespaceDataServiceImpl implements RdfNamespaceDataService {
+
+    private final RdfNamespaceDao rdfNamespaceDao;
+
+    @Autowired
+    public RdfNamespaceDataServiceImpl(RdfNamespaceDao rdfNamespaceDao) {
+        this.rdfNamespaceDao = rdfNamespaceDao;
+    }
+    
+    @Override
+    public List<RdfNamespace> getRdfNamespaces() {
+        return this.rdfNamespaceDao.getRdfNamespaces();
+    }
+    
+}
