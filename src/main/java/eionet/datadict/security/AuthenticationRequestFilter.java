238af2d443f981040e73eebc99337812f257001e
vasilis skiadas <vs@eworx.gr>
2017-03-03 18:11:29 +0200

    - Added additional filter matching cases for security check

--- src/main/java/eionet/datadict/security/AuthenticationRequestFilter.java
+++ src/main/java/eionet/datadict/security/AuthenticationRequestFilter.java
@@ -3,11 +3,9 @@ package eionet.datadict.security;
 import eionet.meta.DDUser;
 import eionet.util.SecurityUtil;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -27,41 +25,36 @@ public class AuthenticationRequestFilter extends UsernamePasswordAuthenticationF
     @Autowired
     private ServletContext servletContext;
 
-    private static final String VOCABULARY_RESOURCE = "vocabulary";
-    private static final String VOCABULARIES_RESOURCE ="vocabularies";
-    private static final Map<String, List<String>> INTERCEPTED_DATADICT_ACTIONS;
-  
-    private static final String GENERIC_DD_UNAUTHORIZED_ACCESS_PAGE_URL="/error.action?type=NOT_AUTHENTICATED_401&message=You+have+to+login+to+access+this+page";
+    private static final List<String> INTERCEPTED_URL_PATTERNS;
+    private static final String GENERIC_DD_UNAUTHORIZED_ACCESS_PAGE_URL = "/error.action?type=NOT_AUTHENTICATED_401&message=You+have+to+login+to+access+this+page";
+
     static {
-        Map<String, List<String>> dataDictActions = new LinkedHashMap<String, List<String>>();
-        dataDictActions.put(VOCABULARY_RESOURCE, Arrays.asList("add","ScheduledJobsQueue","ScheduleSynchronizationView"));
-        dataDictActions.put(VOCABULARIES_RESOURCE, Arrays.asList("maintain"));
-        INTERCEPTED_DATADICT_ACTIONS = Collections.unmodifiableMap(dataDictActions);
+        List<String> interceptedUrlPatterns = new LinkedList<String>();
+        interceptedUrlPatterns.add("delem_attribute.jsp?mode=add");
+        interceptedUrlPatterns.add("attribute/edit/");
+        interceptedUrlPatterns.add("vocabulary?add=");
+        INTERCEPTED_URL_PATTERNS = Collections.unmodifiableList(interceptedUrlPatterns);
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        String requestURL = httpRequest.getRequestURL().toString();
-
-        if (!requiresAuthentication(requestURL, httpRequest)) {
+        if (!isUserAuthenticated(httpRequest)) {
             chain.doFilter(request, response);
             return;
         } else {
-           String contextPath = servletContext.getContextPath();
-            httpServletResponse.sendRedirect(contextPath==null?"":""+contextPath+GENERIC_DD_UNAUTHORIZED_ACCESS_PAGE_URL);
+            String contextPath = servletContext.getContextPath();
+            httpServletResponse.sendRedirect(contextPath == null ? "" : "" + contextPath + GENERIC_DD_UNAUTHORIZED_ACCESS_PAGE_URL);
         }
     }
 
-    private boolean requiresAuthentication(String url, HttpServletRequest request) {
-        if (url.contains(VOCABULARY_RESOURCE)) {
-            List<String> vocabularyActions = INTERCEPTED_DATADICT_ACTIONS.get(VOCABULARY_RESOURCE);
-            for (String vocabularyAction : vocabularyActions) {
-                if (request.getParameter(vocabularyAction)!=null) {
-                    if(getUser(request)==null){
-                     return true; 
-                    }
+    private boolean isUserAuthenticated(HttpServletRequest request) {
+        String completeUrl = this.getFullURL(request);
+        for (String pattern : INTERCEPTED_URL_PATTERNS) {
+            if (completeUrl.contains(pattern)) {
+                if (getUser(request) == null) {
+                    return true;
                 }
             }
         }
@@ -71,5 +64,15 @@ public class AuthenticationRequestFilter extends UsernamePasswordAuthenticationF
     private DDUser getUser(HttpServletRequest request) {
         return SecurityUtil.getUser(request);
     }
-    
+
+    private String getFullURL(HttpServletRequest request) {
+        StringBuffer requestURL = request.getRequestURL();
+        String queryString = request.getQueryString();
+
+        if (queryString == null) {
+            return requestURL.toString();
+        } else {
+            return requestURL.append('?').append(queryString).toString();
+        }
+    }
 }
