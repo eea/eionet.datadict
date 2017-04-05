f9eb2c011d0be1c66bc5f156de00f100d90275dd
vasilis skiadas <vs@eworx.gr>
2017-04-05 16:41:53 +0300

    - Remove unused code + other cosmetic fixes

--- src/main/java/eionet/datadict/services/impl/DataSetServiceImpl.java
+++ src/main/java/eionet/datadict/services/impl/DataSetServiceImpl.java
@@ -2,7 +2,6 @@ package eionet.datadict.services.impl;
 
 import eionet.datadict.dal.AttributeDao;
 import eionet.datadict.dal.AttributeValueDao;
-import eionet.datadict.dal.DataElementDao;
 import eionet.datadict.dal.DatasetDao;
 import eionet.datadict.dal.DatasetTableDao;
 import eionet.datadict.errors.ResourceNotFoundException;
@@ -10,17 +9,10 @@ import eionet.datadict.errors.XmlExportException;
 import eionet.datadict.model.Attribute;
 import eionet.datadict.model.AttributeValue;
 import eionet.datadict.model.DataDictEntity;
-import eionet.datadict.model.DataElement;
 import eionet.datadict.model.DataSet;
 import eionet.datadict.model.DatasetTable;
-import eionet.datadict.model.DatasetTableElement;
 import eionet.datadict.services.DataSetService;
-import eionet.datadict.services.data.NamespaceDataService;
-import eionet.meta.DDSearchEngine;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,12 +25,6 @@ import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Element;
 
 /**
@@ -48,7 +34,6 @@ import org.w3c.dom.Element;
 @Service
 public class DataSetServiceImpl implements DataSetService {
 
-    private final NamespaceDataService namespaceDataService;
     private final DatasetDao datasetDao;
     private final DatasetTableDao datasetTableDao;
     private final AttributeValueDao attributeValueDao;
@@ -72,29 +57,11 @@ public class DataSetServiceImpl implements DataSetService {
     private static final String DOCUMENTATION = "documentation";
     private static final String DEFAULT_XML_LANGUAGE = "en";
 
-    protected DDSearchEngine searchEngine = null;
-    StringBuilder writer = new StringBuilder();
-    private List<String> content = new ArrayList<String>();
-    private List<String> namespaces = new ArrayList<String>();
-    private List<String> imports = new ArrayList<String>();
-
-    private String identitation = "";
     protected String appContext = Props.getRequiredProperty(PropsIF.DD_URL);
     private final static String NS_PREFIX = "xs:";
 
-    protected String lineTerminator = "\n";
-
-    protected String targetNsUrl = "";
-    protected String referredNsPrefix = "";
-    protected String referredNsID = "";
-
-    protected Map<String, String> nonAnnotationAttributes = new HashMap<String, String>();
-
-    private String containerNamespaceID = null;
-
     @Autowired
-    public DataSetServiceImpl(NamespaceDataService namespaceDataService, DatasetDao datasetDao, DatasetTableDao datasetTableDao, AttributeValueDao attributeValueDao, AttributeDao attributeDao) {
-        this.namespaceDataService = namespaceDataService;
+    public DataSetServiceImpl(DatasetDao datasetDao, DatasetTableDao datasetTableDao, AttributeValueDao attributeValueDao, AttributeDao attributeDao) {
         this.datasetDao = datasetDao;
         this.datasetTableDao = datasetTableDao;
         this.attributeValueDao = attributeValueDao;
@@ -177,52 +144,4 @@ public class DataSetServiceImpl implements DataSetService {
     protected String getNamespacePrefix(Namespace ns) {
         return ns == null ? "dd" : "dd" + ns.getId();
     }
-
-    private void transformAndPrintDocument(Document doc) throws TransformerException {
-
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-        DOMSource source = new DOMSource(doc);
-        StreamResult result = new StreamResult(System.out);
-        // If we wanted to write it to file:
-        //		StreamResult result = new StreamResult(new File("C:\\file.xml"));
-        transformer.transform(source, result);
-    }
-
-    private static class NameTypeElementMaker {
-
-        private String nsPrefix;
-        private Document doc;
-
-        public NameTypeElementMaker(String nsPrefix, Document doc) {
-            this.nsPrefix = nsPrefix;
-            this.doc = doc;
-        }
-
-        public Element createElement(String elementName, String nameAttrVal, String typeAttrVal) {
-            Element element = doc.createElementNS(XMLConstants.W3C_XML_SCHEMA_NS_URI, nsPrefix + elementName);
-            if (nameAttrVal != null) {
-                element.setAttribute("name", nameAttrVal);
-            }
-            if (typeAttrVal != null) {
-                element.setAttribute("type", typeAttrVal);
-            }
-            return element;
-        }
-
-        public Element createElement(String elementName, String nameAttrVal) {
-            return createElement(elementName, nameAttrVal, null);
-        }
-
-        public Element createElement(String elementName) {
-            return createElement(elementName, null, null);
-        }
-    }
-
 }
