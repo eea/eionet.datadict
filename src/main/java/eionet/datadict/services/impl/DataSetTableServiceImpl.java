3e7f0fe0087013c23816b7ac5f41e78aa3bbdda4
vasilis skiadas <vs@eworx.gr>
2017-05-10 15:32:10 +0300

    - Fixes in Namespace of attributes

--- src/main/java/eionet/datadict/services/impl/DataSetTableServiceImpl.java
+++ src/main/java/eionet/datadict/services/impl/DataSetTableServiceImpl.java
@@ -59,19 +59,19 @@ public class DataSetTableServiceImpl implements DataSetTableService {
     private static final String ELEMENT = "element";
     private static final String ANNOTATION = "annotation";
     private static final String COMPLEX_TYPE = "complexType";
-    private static final String SIMPLE_TYPE ="simpleType";
-        private static final String RESTRICTION ="restriction";
+    private static final String SIMPLE_TYPE = "simpleType";
+    private static final String RESTRICTION = "restriction";
 
     private static final String SEQUENCE = "sequence";
     private static final String REF = "ref";
     private static final String DOCUMENTATION = "documentation";
     private static final String DEFAULT_XML_LANGUAGE = "en";
     private static final String NAME = "name";
-    protected String appContext = Props.getRequiredProperty(PropsIF.DD_URL);
+    protected static String appContext = Props.getRequiredProperty(PropsIF.DD_URL);
     private final static String NS_PREFIX = "xs:";
-   private static final String BASE="base";
-    
-    
+    private static final String BASE = "base";
+    private static final String ISOATTRS_NAMESPACE=appContext + "/" + Namespace.URL_PREFIX + "/" + ISOATTRS_NAMESPACE_ID;
+    private static final String DDATTRS_NAMESPACE=appContext + "/" + Namespace.URL_PREFIX + "/" + DDATTRS_NAMESPACE_ID;
     @Autowired
     public DataSetTableServiceImpl(DatasetTableDao datasetTableDao, DataElementDao dataElementDao, AttributeValueDao attributeValueDao, AttributeDao attributeDao, DatasetDao datasetDao, DatasetTableDataService datasetTableDataService) {
         this.datasetTableDao = datasetTableDao;
@@ -82,15 +82,13 @@ public class DataSetTableServiceImpl implements DataSetTableService {
         this.datasetTableDataService = datasetTableDataService;
     }
 
-    
-    
-    
     @Override
     public Document getDataSetTableXMLSchema(int id) throws XmlExportException {
 
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = null;
         DatasetTable dataSetTable = this.datasetTableDao.getById(id);
+        
         try {
             docBuilder = docFactory.newDocumentBuilder();
             Document doc = docBuilder.newDocument();
@@ -100,8 +98,8 @@ public class DataSetTableServiceImpl implements DataSetTableService {
                     "xsi:schemaLocation", "http://www.w3.org/2001/XMLSchema http://www.w3.org/2001/XMLSchema.xsd");
             schemaRoot.setAttribute("xmlns:xs", "http://www.w3.org/2001/XMLSchema");
             schemaRoot.setAttribute("xmlns", appContext + "/" + Namespace.URL_PREFIX + "/" + dataSetTable.getCorrespondingNS().getId());
-            schemaRoot.setAttribute("xmlns:isoattrs", appContext + "/" + Namespace.URL_PREFIX + "/" + ISOATTRS_NAMESPACE_ID);
-            schemaRoot.setAttribute("xmlns:ddattrs", appContext + "/" + Namespace.URL_PREFIX + "/" + DDATTRS_NAMESPACE_ID);
+            schemaRoot.setAttribute("xmlns:isoattrs",ISOATTRS_NAMESPACE);
+            schemaRoot.setAttribute("xmlns:ddattrs", DDATTRS_NAMESPACE);
             schemaRoot.setAttribute(TARGET_NAMESPACE, appContext + "/" + Namespace.URL_PREFIX + "/" + dataSetTable.getCorrespondingNS().getId());
             schemaRoot.setAttribute("elementFormDefault", "qualified");
             schemaRoot.setAttribute("attributeFormDefault", "unqualified");
@@ -110,73 +108,72 @@ public class DataSetTableServiceImpl implements DataSetTableService {
             DataSet dataSet = datasetDao.getById(datasetId);
             Element tableRootElement = elMaker.createElement("element", dataSetTable.getShortName());
 
-                 
-            
             try {
                 DatasetTable dsTableFull = this.datasetTableDataService.getFullDatasetTableDefinition(id);
             } catch (ResourceNotFoundException ex) {
                 Logger.getLogger(DataSetTableServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
             }
-             schemaRoot.appendChild(tableRootElement);
+            schemaRoot.appendChild(tableRootElement);
             Element dsAnnotation = elMaker.createElement(ANNOTATION);
             tableRootElement.appendChild(dsAnnotation);
             Element dsDocumentation = elMaker.createElement(DOCUMENTATION);
             dsDocumentation.setAttribute("xml:lang", DEFAULT_XML_LANGUAGE);
-            dsAnnotation.appendChild(dsDocumentation); 
+            dsAnnotation.appendChild(dsDocumentation);
 
             List<Attribute> dataSetTableAttributes = attributeDao.getByDataDictEntity(new DataDictEntity(dataSetTable.getId(), DataDictEntity.Entity.T));
             for (Attribute dataSetTableAttribute : dataSetTableAttributes) {
                 AttributeValue attributeValue = attributeValueDao.getByAttributeAndEntityId(dataSetTableAttribute.getId(), dataSetTable.getId());
-                        Element attributeElement = elMaker.createElement(dataSetTableAttribute.getNamespace().getShortName().replace("_", ""), dataSetTableAttribute.getShortName().replace(" ", ""));
-                        if(attributeValue!=null){
-                        attributeElement.appendChild(doc.createTextNode(attributeValue.getValue()));
-                        }
-                        dsDocumentation.appendChild(attributeElement);
+              //  Element attributeElement = elMaker.createElement(dataSetTableAttribute.getNamespace().getShortName().replace("_", ""),null, dataSetTableAttribute.getShortName().replace(" ", ""));
+                            Element attributeElement = elMaker.createElement(dataSetTableAttribute.getShortName().replace(" ", ""),null,dataSetTableAttribute.getNamespace().getShortName().replace("_", "") );
+
+                if (attributeValue != null) {
+                    attributeElement.appendChild(doc.createTextNode(attributeValue.getValue()));
+                }
+                dsDocumentation.appendChild(attributeElement);
             }
             List<Attribute> dataSetAttributes = attributeDao.getByDataDictEntity(new DataDictEntity(datasetId, DataDictEntity.Entity.DS));
             List<AttributeValue> dataSetAttributesValues = new ArrayList<AttributeValue>();
-             for (Attribute dataSetAttribute : dataSetAttributes) {
+            for (Attribute dataSetAttribute : dataSetAttributes) {
                 AttributeValue attributeValue = attributeValueDao.getByAttributeAndEntityId(dataSetAttribute.getId(), datasetId);
                 dataSetAttributesValues.add(attributeValue);
-                        Element attributeElement = elMaker.createElement(dataSetAttribute.getNamespace().getShortName().replace("_", ""), dataSetAttribute.getShortName().replace(" ", ""));
-                        if(attributeValue!=null){
-                        attributeElement.appendChild(doc.createTextNode(attributeValue.getValue()));
-                        }
-                        dsDocumentation.appendChild(attributeElement);
+                Element attributeElement = elMaker.createElement(dataSetAttribute.getShortName().replace(" ", ""),null,dataSetAttribute.getNamespace().getShortName().replace("_", "") );
+                if (attributeValue != null) {
+                    attributeElement.appendChild(doc.createTextNode(attributeValue.getValue()));
+                }
+                dsDocumentation.appendChild(attributeElement);
             }
-         
-         Element complexType = elMaker.createElement(COMPLEX_TYPE);
-        
-         tableRootElement.appendChild(complexType);
-        
-         Element sequence = elMaker.createElement(SEQUENCE);
+
+            Element complexType = elMaker.createElement(COMPLEX_TYPE);
+
+            tableRootElement.appendChild(complexType);
+
+            Element sequence = elMaker.createElement(SEQUENCE);
             complexType.appendChild(sequence);
-                Element rowElement = elMaker.createElement(ELEMENT);
-                rowElement.setAttribute(NAME, "Row");
-                rowElement.setAttribute("minOccurs", "1");
-                rowElement.setAttribute("maxOccurs", "unbounded");
-                sequence.appendChild(rowElement);
-          Element rowComplexType = elMaker.createElement(COMPLEX_TYPE);
-             rowElement.appendChild(rowComplexType);
-              Element rowSequence = elMaker.createElement(SEQUENCE);
-              rowComplexType.appendChild(rowSequence);
-              
+            Element rowElement = elMaker.createElement(ELEMENT);
+            rowElement.setAttribute(NAME, "Row");
+            rowElement.setAttribute("minOccurs", "1");
+            rowElement.setAttribute("maxOccurs", "unbounded");
+            sequence.appendChild(rowElement);
+            Element rowComplexType = elMaker.createElement(COMPLEX_TYPE);
+            rowElement.appendChild(rowComplexType);
+            Element rowSequence = elMaker.createElement(SEQUENCE);
+            rowComplexType.appendChild(rowSequence);
+
             for (DataElement dataElement : dataElements) {
-                Element tableElement = elMaker.createElement( ELEMENT);
+                Element tableElement = elMaker.createElement(ELEMENT);
                 tableElement.setAttribute(REF, dataElement.getShortName());
                 tableElement.setAttribute("minOccurs", "1");
                 tableElement.setAttribute("maxOccurs", "1");
                 rowSequence.appendChild(tableElement);
             }
-            
 
             for (DataElement dataElement : dataElements) {
                 Element xmlElement = elMaker.createElement("element", dataElement.getShortName());
-                String MinSize="";
-                String MaxSize="";
-                String Datatype ="";
-                String MinInclusiveValue="";
-                String MaxInclusiveValue="";
+                String MinSize = "";
+                String MaxSize = "";
+                String Datatype = "";
+                String MinInclusiveValue = "";
+                String MaxInclusiveValue = "";
 
                 schemaRoot.appendChild(xmlElement);
                 Element elemAnnotation = elMaker.createElement(ANNOTATION);
@@ -188,66 +185,61 @@ public class DataSetTableServiceImpl implements DataSetTableService {
                 attributeValues.addAll(dataSetAttributesValues);
                 for (AttributeValue attributeValue : attributeValues) {
                     Attribute attribute = attributeDao.getById(attributeValue.getAttributeId());
-                   if(attribute.getShortName().equals("MinSize")){
-                    MinSize=attributeValue.getValue();
-                    continue;
-                   }
-                   if(attribute.getShortName().equals("MaxSize")){
-                    MaxSize=attributeValue.getValue();
-                    continue;
-                   }
-                   if(attribute.getShortName().equals("Datatype")){
-                    Datatype=attributeValue.getValue();
-                    continue;
-                   }
-                  if(attribute.getShortName().equals("MinInclusiveValue")){
-                    MinInclusiveValue =attributeValue.getValue();
-                    continue;
-                   }
-                  if(attribute.getShortName().equals("MaxInclusiveValue")){
-                    MaxInclusiveValue =attributeValue.getValue();
-                    continue;
-                   }
-                    Element attributeElement = elMaker.createElement(attribute.getNamespace().getShortName().replace("_", ""), attribute.getShortName().replace(" ", ""));
+                    if (attribute.getShortName().equals("MinSize")) {
+                        MinSize = attributeValue.getValue();
+                        continue;
+                    }
+                    if (attribute.getShortName().equals("MaxSize")) {
+                        MaxSize = attributeValue.getValue();
+                        continue;
+                    }
+                    if (attribute.getShortName().equals("Datatype")) {
+                        Datatype = attributeValue.getValue();
+                        continue;
+                    }
+                    if (attribute.getShortName().equals("MinInclusiveValue")) {
+                        MinInclusiveValue = attributeValue.getValue();
+                        continue;
+                    }
+                    if (attribute.getShortName().equals("MaxInclusiveValue")) {
+                        MaxInclusiveValue = attributeValue.getValue();
+                        continue;
+                    }
+                    Element attributeElement = elMaker.createElement(attribute.getShortName().replace(" ", ""),null, attribute.getNamespace().getShortName().replace("_", ""));
                     attributeElement.appendChild(doc.createTextNode(attributeValue.getValue()));
                     elemDocumentation.appendChild(attributeElement);
                 }
                 Element dataElementSimpleType = elMaker.createElement(SIMPLE_TYPE);
                 xmlElement.appendChild(dataElementSimpleType);
                 Element dataElementRestriction = elMaker.createElement(RESTRICTION);
-                dataElementRestriction.setAttribute(BASE, NS_PREFIX+Datatype);
+                dataElementRestriction.setAttribute(BASE, NS_PREFIX + Datatype);
                 dataElementSimpleType.appendChild(dataElementRestriction);
-                if(Datatype.equals("decimal")){
-                  Element totalDigitsElement = elMaker.createElement("totalDigits");
-                  totalDigitsElement.setAttribute("value", MaxSize);
-                  dataElementRestriction.appendChild(totalDigitsElement);
-                  Element minInclusiveElement = elMaker.createElement("minInclusive");
-                  minInclusiveElement.setAttribute("value", MinInclusiveValue);
-                                    dataElementRestriction.appendChild(minInclusiveElement);
-
-                  
-                  Element maxInclusiveElement = elMaker.createElement("maxInclusive");
-                  maxInclusiveElement.setAttribute("value", MaxInclusiveValue);
-                                    dataElementRestriction.appendChild(maxInclusiveElement);
+                if (Datatype.equals("decimal")) {
+                    Element totalDigitsElement = elMaker.createElement("totalDigits");
+                    totalDigitsElement.setAttribute("value", MaxSize);
+                    dataElementRestriction.appendChild(totalDigitsElement);
+                    Element minInclusiveElement = elMaker.createElement("minInclusive");
+                    minInclusiveElement.setAttribute("value", MinInclusiveValue);
+                    dataElementRestriction.appendChild(minInclusiveElement);
+                    Element maxInclusiveElement = elMaker.createElement("maxInclusive");
+                    maxInclusiveElement.setAttribute("value", MaxInclusiveValue);
+                    dataElementRestriction.appendChild(maxInclusiveElement);
+                }
+                if (Datatype.equals("integer")) {
+                    Element totalDigitsElement = elMaker.createElement("totalDigits");
+                    totalDigitsElement.setAttribute("value", MaxSize);
+                    dataElementRestriction.appendChild(totalDigitsElement);
+                }
+                if (Datatype.equals("string")) {
+                    Element minLengthElement = elMaker.createElement("minLength");
+                    minLengthElement.setAttribute("value", MinSize);
+                    dataElementRestriction.appendChild(minLengthElement);
+                    Element maxLengthElement = elMaker.createElement("maxLength");
+                    maxLengthElement.setAttribute("value", MaxSize);
+                    dataElementRestriction.appendChild(maxLengthElement);
 
                 }
-                if(Datatype.equals("integer")){
-                  Element totalDigitsElement = elMaker.createElement("totalDigits");
-                  totalDigitsElement.setAttribute("value", MaxSize);
-                  dataElementRestriction.appendChild(totalDigitsElement);
-                }
-                if(Datatype.equals("string")){
-                  Element minLengthElement = elMaker.createElement("minLength");
-                  minLengthElement.setAttribute("value", MinSize);
-                                                      dataElementRestriction.appendChild(minLengthElement);
 
-                  Element maxLengthElement = elMaker.createElement("maxLength");
-                  maxLengthElement.setAttribute("value", MaxSize);
-                
-                                                      dataElementRestriction.appendChild(maxLengthElement);
-
-                }
-             
             }
 
             doc.appendChild(schemaRoot);
@@ -268,8 +260,18 @@ public class DataSetTableServiceImpl implements DataSetTableService {
             this.doc = doc;
         }
 
-        public Element createElement(String elementName, String nameAttrVal, String typeAttrVal) {
-            Element element = doc.createElementNS(XMLConstants.W3C_XML_SCHEMA_NS_URI, nsPrefix + elementName);
+        public Element createElement(String elementName, String nameAttrVal, String typeAttrVal, String nameSpacePrefix) {
+            Element element;
+            if(nameSpacePrefix!=null &&nameSpacePrefix.equals("isoattrs")) {
+                element = doc.createElementNS( ISOATTRS_NAMESPACE, nameSpacePrefix +":"+ elementName);
+            }else if(nameSpacePrefix!=null &&nameSpacePrefix.equals("ddattrs")) {
+                element = doc.createElementNS(DDATTRS_NAMESPACE, nameSpacePrefix +":"+ elementName);
+            }
+            else  {
+                element = doc.createElementNS(XMLConstants.W3C_XML_SCHEMA_NS_URI, nsPrefix + elementName);
+
+            }
+
             if (nameAttrVal != null) {
                 element.setAttribute("name", nameAttrVal);
             }
@@ -280,12 +282,17 @@ public class DataSetTableServiceImpl implements DataSetTableService {
         }
 
         public Element createElement(String elementName, String nameAttrVal) {
-            return createElement(elementName, nameAttrVal, null);
+            return createElement(elementName, nameAttrVal, null, null);
+        }
+
+        public Element createElement(String elementName, String nameAttrVal, String nameSpacePrefix) {
+            return createElement(elementName, nameAttrVal, null, nameSpacePrefix);
         }
 
         public Element createElement(String elementName) {
-            return createElement(elementName, null, null);
+            return createElement(elementName, null, null, null);
         }
+
     }
 
 }
