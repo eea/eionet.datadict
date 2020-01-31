3b39c10ea70fa1977ffbe77084e19b59a52326a6
deniaAthanasakou <antonia.ath@gmail.com>
2020-01-31 13:46:10 +0200

    Small fix for USER_MODIFIED. Refs #111702

--- src/main/java/eionet/web/action/VocabularyFolderApiActionBean.java
+++ src/main/java/eionet/web/action/VocabularyFolderApiActionBean.java
@@ -435,7 +435,7 @@ public class VocabularyFolderApiActionBean extends AbstractActionBean {
             }
 
             Date dateModified = new Date();
-            String userModified = Props.getRequiredProperty(PropsIF.API_USER_MODIFIED_IDENTIFIER);
+            String userModified = PropsIF.API_USER_MODIFIED_IDENTIFIER;
             vocabularyFolderDAO.updateDateAndUserModified(dateModified, userModified, vocabularyFolder.getId());
             LOGGER.info("uploadRdf API - DATE_MODIFIED was updated");
 
