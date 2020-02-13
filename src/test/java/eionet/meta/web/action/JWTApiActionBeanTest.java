807e154b26ad6801b61e82d9530bb8290104fecf
deniaAthanasakou <antonia.ath@gmail.com>
2020-02-13 11:37:12 +0200

    Fixes for tests. Refs #114189

--- src/test/java/eionet/meta/web/action/JWTApiActionBeanTest.java
+++ src/test/java/eionet/meta/web/action/JWTApiActionBeanTest.java
@@ -5,14 +5,21 @@ import eionet.datadict.services.JWTService;
 import eionet.meta.service.ServiceException;
 import eionet.web.DDActionBeanContext;
 import eionet.web.action.JWTApiActionBean;
-import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.StreamingResolution;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
@@ -40,6 +47,7 @@ public class JWTApiActionBeanTest {
         when(jwtApiActionBean.getJwtService().generateJWTToken()).thenReturn(expectedToken);
         when(jwtApiActionBean.generateJWTToken()).thenCallRealMethod();
         when(jwtApiActionBean.isPostRequest()).thenCallRealMethod();
+        when(jwtApiActionBean.getContext()).thenReturn(ctx);
         when(ctx.getRequest()).thenReturn(request);
     }
 
@@ -80,13 +88,71 @@ public class JWTApiActionBeanTest {
         fail("No parameters in request - exception did not throw!");
     }
 
+    //TODO complete following tests after finding way to authenticate user
+
+    /* Test case: the given credentials do not belong to an eionet user */
+  /*  @Test(expected = ServiceException.class)
+    public void testGenerateJWTTokenUserDoesNotExist() throws Exception {
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put("username", "falseUsername");
+        credentials.put("password", "falsePassword");
+        request.setRequestURI("/api/jwt/generateJWTToken");
+        request.setMethod("POST");
+        request.setParameters(credentials);
+
+        try
+        {
+            jwtApiActionBean.generateJWTToken();
+        }
+        catch(ServiceException se)
+        {
+            String expectedMessage = "generateJWTToken API - Wrong credentials were retrieved.";
+            Assert.assertThat(se.getMessage(), is(expectedMessage));
+            throw se;
+        }
+        fail("Wrong credentials - exception did not throw!");
+    }
+    */
+
+
+    /* Test case: the user does not have admin rights */
+   /* @Test(expected = ServiceException.class)
+    public void testGenerateJWTTokenUserIsNotAdmin() throws Exception {
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put("username", "notAdminUsername");
+        credentials.put("password", "notAdminPassword");
+        request.setRequestURI("/api/jwt/generateJWTToken");
+        request.setMethod("POST");
+        request.setParameters(credentials);
+
+        try
+        {
+            jwtApiActionBean.generateJWTToken();
+        }
+        catch(ServiceException se)
+        {
+            String expectedMessage = "generateJWTToken API - Wrong credentials were retrieved.";
+            Assert.assertThat(se.getMessage(), is(expectedMessage));
+            throw se;
+        }
+        fail("User is not admin - exception did not throw!");
+    }*/
+
     /* Test case: successful generation of token */
     @Test
     public void testGenerateJWTTokenSuccessful() throws Exception {
-        //String actualToken = jwtApiActionBean.generateJWTToken();
-        //Assert.assertThat(actualToken, is(expectedToken));
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put("username", "adminUsername");
+        credentials.put("password", "adminPassword");
+        request.setRequestURI("/api/jwt/generateJWTToken");
+        request.setMethod("POST");
+        request.setParameters(credentials);
+
+        Resolution result = jwtApiActionBean.generateJWTToken();
+
+        Assert.assertThat(result, is(notNullValue()));
+        assertEquals(result.getClass(), StreamingResolution.class);
+
     }
-
-
 
 }
