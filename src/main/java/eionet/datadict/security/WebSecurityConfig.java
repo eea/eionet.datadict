7c37c603890dc6a987c9fe13f130c0733c139ca4
js <js@eworx.gr>
2022-08-09 14:57:17 +0300

    allow encoded percent in spring's firewall (urls are encoded through stripes). Refs #152047

--- src/main/java/eionet/datadict/security/WebSecurityConfig.java
+++ src/main/java/eionet/datadict/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package eionet.datadict.security;
 
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 /**
  *
@@ -38,4 +42,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 antMatchers("/images/**");
     }
 
+    @Bean
+    public HttpFirewall configureFirewall() {
+        StrictHttpFirewall strictHttpFirewall = new StrictHttpFirewall();
+        strictHttpFirewall.setAllowUrlEncodedPercent(true);
+        return strictHttpFirewall;
+    }
 }
