6de67464a045897143e67eabf54a46f8afb1be8c
js <js@eworx.gr>
2025-06-13 14:39:57 +0300

    update to commons dbcp 2.x version

--- src/test/java/eionet/DataSourceSupport.java
+++ src/test/java/eionet/DataSourceSupport.java
@@ -1,14 +1,13 @@
 package eionet;
 
-import java.io.InputStream;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Properties;
-
-import javax.sql.DataSource;
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Support functions to set up MySQL data source
--- src/test/java/eionet/datadict/dal/connection/MysqlSystemDatabase.java
+++ src/test/java/eionet/datadict/dal/connection/MysqlSystemDatabase.java
@@ -1,6 +1,6 @@
 package eionet.datadict.dal.connection;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.stereotype.Component;
 
 @Component
