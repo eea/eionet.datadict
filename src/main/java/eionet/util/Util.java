931eb747874105caaa11a267634306793ed4cb7c
js <js@eworx.gr>
2026-05-20 15:02:35 +0300

    import fix

--- src/main/java/eionet/util/Util.java
+++ src/main/java/eionet/util/Util.java
@@ -28,7 +28,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
